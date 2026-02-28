29252ba4475b3df4412df90a6d9206c2ee0357ac
WatchVIM <abawn@watchvim.us>
2026-02-27 22:41:03 -0700

    Update channels.ts

--- lib/channels.ts
+++ lib/channels.ts
@@ -1,4 +1,4 @@
 export const CHANNEL_IDS: string[] = [
-  // Replace with your YouTube channel IDs (UC....)
-  "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+  // Use a YouTube handle (recommended)
+  "@InclusiveQuest",
 ];
